4e1cd84757dcf09aeae763e92c305052076309e9
Alexander <almerodevelop@gmail.com>
2020-04-18 14:38:56 +0300

    Публикация на гите
    html,css,js

---
+++ bitrix/cache/js/s1/stroykas_main/template_604af17dae08f85b0c9ea1b971b0cfc1/template_604af17dae08f85b0c9ea1b971b0cfc1_v1.js
@@ -0,0 +1,7 @@
+
+; /* Start:"a:4:{s:4:"full";s:92:"/bitrix/templates/stroykas_main/components/bitrix/menu/top_multi/script.min.js?1563564511407";s:6:"source";s:74:"/bitrix/templates/stroykas_main/components/bitrix/menu/top_multi/script.js";s:3:"min";s:78:"/bitrix/templates/stroykas_main/components/bitrix/menu/top_multi/script.min.js";s:3:"map";s:78:"/bitrix/templates/stroykas_main/components/bitrix/menu/top_multi/script.map.js";}"*/
+var jshover=function(){var e=document.getElementById("horizontal-multilevel-menu");if(!e)return;var t=e.getElementsByTagName("li");for(var n=0;n<t.length;n++){t[n].onmouseover=function(){this.className+=" jshover"};t[n].onmouseout=function(){this.className=this.className.replace(new RegExp(" jshover\\b"),"")}}};if(window.attachEvent)window.attachEvent("onload",jshover);
+/* End */
+;; /* /bitrix/templates/stroykas_main/components/bitrix/menu/top_multi/script.min.js?1563564511407*/
+
+//# sourceMappingURL=template_604af17dae08f85b0c9ea1b971b0cfc1.map.js
